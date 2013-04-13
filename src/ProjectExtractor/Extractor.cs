d2c05349385f95e15984b1d58ec422b7e80cf097
Brian Scaturro <scaturrob@gmail.com>
2013-04-13 13:29:14 -0400

    Extractor checks for valid zip file

---
+++ src/ProjectExtractor/Extractor.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace ProjectExtractor
+{
+    public class Extractor
+    {
+        public string ZipPath { get; private set; }
+
+        public Extractor(string zipPath)
+        {
+            if (!File.Exists(zipPath))
+                throw new FileNotFoundException("Solution file not found");
+
+            ZipPath = zipPath;
+        }
+    }
+}
---
+++ src/Test.Unit/ProjectExtractor/ExtractorTest.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using NUnit.Framework;
+using ProjectExtractor;
+
+namespace Test.Unit.ProjectExtractor
+{
+    [TestFixture]
+    public class ExtractorTest
+    {
+        protected string ZipPath { get; set; }
+        protected Extractor Extractor { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var fixturesPath = Path.GetFullPath("Fixtures");
+            ZipPath = fixturesPath + Path.DirectorySeparatorChar + "ndriven-master.zip";
+            Extractor = new Extractor(ZipPath);
+        }
+
+        [Test]
+        [ExpectedException(typeof (FileNotFoundException))]
+        public void Extractor_throws_exception_if_zip_file_does_not_exist()
+        {
+            var extractor = new Extractor("nope");
+        }
+
+        [Test]
+        public void Constructor_should_set_zip_path()
+        {
+            Assert.AreEqual(ZipPath, Extractor.ZipPath);
+        }
+    }
+}
